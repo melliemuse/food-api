3f8938493b69c87b5b24c4d8f83d7fa73240a069
Melody Stern <melliemuse@gmail.com>
2019-10-17 13:00:00 -0500

    updated files with completed exercise

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,4 +1,39 @@
 
+
+// Chained together
+// fetch gets data
+// fetch("http://localhost:8088/food")
+//     .then(foods => foods.json())
+//     .then(parsedFoods => {
+//         parsedFoods.forEach(food => {
+            
+//         let foodAsHTML = foodFactory(food)
+            
+//         console.log(foodFactory(food))
+//         addFoodToDom(foodAsHTML)
+//         })
+//     })
+
+   
+// Once you have the barcode, you can use the Open Food Facts JSON API to get a plethora of information about that product. For example, the barcode for Jimmy Dean Pork Sausage is 0077900110726. I can take that barcode and query their API.
+  
+    // fetch("https://world.openfoodfacts.org/api/v0/product/0011150479547.json")
+    // .then(response => response.json())
+    // .then(productInfo => {
+    //     0077900110726 
+    //     console.log(productInfo);
+    // })
+
+//     Your job is to query the Open Food Facts API for each of your products, and list the following additional information.
+
+// Ingredients
+// Country of origin
+// Calories per serving
+// Fat per serving
+// Sugar per serving
+// Helpful hints: You will need to use the forEach array method to iterate your foods. Inside that forEach, you will need to perform another fetch to get the additional information. The barcode value must be interpolated inside the URL for the inner fetch.
+
+
  // Create a function which returns a string template. The template is the HTML representation for a food item.
 
  function foodFactory (food) {
@@ -7,7 +42,11 @@
     <h1>${food.name}</h1>
     <section>${food.category}</section>
     <section>${food.ethnicity}</section>
-    <section>${food.id}</section>
+    <section>${food.ingredients}<section>
+    <section>${food.country}<section>
+    <section>${food.calories}<section>
+    <section>${food.fat}<section>
+    <section>${food.sugar}<section>
     </div>
     `;
 }
@@ -20,23 +59,50 @@ const addFoodToDom = (foodAsHTML) => {
     return div; 
 }
 
-// Chained together
-// fetch gets data
-fetch("http://localhost:8088/food")
-    .then(foods => foods.json())
-    .then(parsedFoods => {
-        parsedFoods.forEach(food => {
-            
-        let foodAsHTML = foodFactory(food)
-            
-        console.log(foodFactory(food))
-        addFoodToDom(foodAsHTML)
+ 
+    fetch("http://localhost:8088/food")
+    .then(response => response.json())
+    .then(myParsedFoods => {
+        myParsedFoods.forEach(food => {
+            console.log(food) // Should have a `barcode` property
+
+    //         // Now fetch the food from the Food API
+            fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
+                .then(response => response.json())
+                .then(productInfo => {
+                    const evaluateStoreProperties = (propertyJson, propertyStore) => {
+                        if (productInfo.product[propertyJson]) {
+                            food[propertyStore] = productInfo.product[propertyJson]
+                          } else {
+                            food[propertyStore] = `no ${propertyStore} listed`
+                          }
+
+                        }
+                        evaluateStoreProperties('ingredients_text', 'ingredients')
+                        evaluateStoreProperties('countries', 'country')
+                        evaluateStoreProperties('calories', 'calories')
+                     
+                       if (productInfo.product.nutriments.fat) {
+                           food.fat = productInfo.product.nutriments.fat
+                       } else {
+                           food.fat = `no fat listed`
+                   }
+
+                       if (productInfo.product.nutriments.sugars) {
+                           food.sugar = productInfo.product.nutriments.sugars
+                        } else {
+                           food.sugar = `no sugar listed`
+                   }
+                     
+
+                    
+                    // console.log(food.barcode)
+
+    // //                 // Produce HTML representation
+                    const foodAsHTML = foodFactory(food)
+
+    // //                 // Add representation to DOM
+                    addFoodToDom(foodAsHTML)
+                })
         })
     })
-
-   
-
-
- 
-
-  
